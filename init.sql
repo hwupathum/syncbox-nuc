b762b5f6efee512f20fe97528c9ca050589d6739
Udara <hwupathum@gmail.com>
2022-01-08 22:59:25 +0530

    first commit

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS syncbox;
+
+USE syncbox;
+
+CREATE TABLE IF NOT EXISTS users (
+    userid INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    scope VARCHAR(50) NOT NULL,
+    PRIMARY KEY (userid)
+);
