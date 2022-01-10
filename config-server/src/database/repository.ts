ef1da78a242e28991b88ddf27919f3acbebdbe5e
Melanga Kasun <melangakasun@gmail.com>
2022-01-11 01:58:46 +0530

    Introduced the update password query

--- config-server/src/database/repository.ts
+++ config-server/src/database/repository.ts
@@ -9,3 +9,8 @@ export function getUserByUsername(username: string, callback: Function) {
     let sql_query = `SELECT * FROM users WHERE username = '${username}'`;
     connection.query(sql_query, (error, result, fields) => callback(error, result, fields));
 }
+
+export function updateUserPasswordByUsername(username: string, password: string, callback: Function) {
+    let sql_query = 'UPDATE users SET password = ? WHERE username = ?';
+    connection.query(sql_query, [password, username], (error, result, fields) => callback(error, result, fields));
+}
