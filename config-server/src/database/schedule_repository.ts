b9fbb7697394eb0b801dac89be6a9f6c56f4438a
Melanga Kasun <melangakasun@gmail.com>
2022-01-11 20:44:03 +0530

    Introduced the get all schedules query

--- config-server/src/database/schedule_repository.ts
+++ config-server/src/database/schedule_repository.ts
@@ -4,3 +4,8 @@ export function createNewSchedule(username: string, filename: string, start_date
     let sql_query = 'INSERT INTO schedules SET ?';
     connection.query(sql_query, { username, filename, start_date, start_time }, (error, result, fields) => callback(error, result, fields));
 }
+
+export function getAllSchedules(callback: Function) {
+    let sql_query = 'SELECT * FROM schedules';
+    connection.query(sql_query, (error, result, fields) => callback(error, result, fields));
+}
