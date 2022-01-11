fd186e3b89169989f4fa6f81bda1668a9d60bf14
Melanga Kasun <melangakasun@gmail.com>
2022-01-11 20:52:31 +0530

    Renamed the columns

--- init.sql
+++ init.sql
@@ -11,10 +11,10 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS schedules (
-    scheduleId INT NOT NULL AUTO_INCREMENT,
+    schedule_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     filename VARCHAR(255) NOT NULL,
-    startDate VARCHAR(20) NOT NULL,
-    startTime VARCHAR(10) NOT NULL,
+    start_date VARCHAR(20) NOT NULL,
+    start_time VARCHAR(10) NOT NULL,
     PRIMARY KEY (scheduleId)
 );
