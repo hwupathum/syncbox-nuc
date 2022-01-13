269d93c479cea19be0f3a2c11005558135e966bb
Udara <hwupathum@gmail.com>
2022-01-13 09:13:29 +0530

    init.sql changes

--- init.sql
+++ init.sql
@@ -5,15 +5,15 @@ USE syncbox;
 CREATE TABLE IF NOT EXISTS users (
     user_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
-    password LONGTEXT NOT NULL,
+    password VARCHAR(255) NOT NULL,
     scope VARCHAR(255) NOT NULL,
     PRIMARY KEY (userid)
 );
 
 CREATE TABLE IF NOT EXISTS schedules (
     schedule_id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NUL NOT NULL,
-    filename VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    full_path TEXT NOT NULL,
     start_time DATETIME NOT NULL,
     PRIMARY KEY (schedule_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
@@ -21,8 +21,9 @@ CREATE TABLE IF NOT EXISTS schedules (
 
 CREATE TABLE IF NOT EXISTS logs (
     log_id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NUL NOT NULL,
-    path LONGTEXT NOT NULL,
+    user_id INT NOT NULL,
+    filename VARCHAR(255) NOT NULL,
+    path TEXT NOT NULL,
     extension VARCHAR(5) NOT NULL,
     acess_time DATETIME DEFAULT GETDATE() NOT NULL,
     PRIMARY KEY (log_id),
