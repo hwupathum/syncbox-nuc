163e6d55877ec8266519d576e1b71183f5938809
Melanga Kasun <melangakasun@gmail.com>
2022-02-28 23:42:30 +0530

    Updated the script with access time

--- list.py
+++ list.py
@@ -4,13 +4,20 @@ import os
 from datetime import datetime
 import mysql.connector
 
-def updatelog(username, filename, path, parent):
+def updatelog(id, username, filename, path, parent):
+    mycursor.execute("SELECT full_path, access_time FROM files WHERE user_id = " + str(id))
+    files = mycursor.fetchall()
+
+    files_dic = {}
+    for full_path, access_time in files:
+        files_dic[full_path] = int(access_time.timestamp())
+
     with open(filename, "a", encoding="utf-8", errors="backslashreplace") as file_object:
         for path, subdirs, files in os.walk(path):
             for name in files:
                 full_path = os.path.join(path, name)
                 info = os.stat(full_path)
-                info_tuple = (username, datetime.date(datetime.now()), full_path.replace(parent, ""), info.st_size, int(info.st_mtime), info.st_atime, info.st_ino)
+                info_tuple = (username, datetime.date(datetime.now()), full_path.replace(parent, ""), info.st_size, int(info.st_mtime), files_dic[full_path.replace(parent, "")], info.st_ino)
                 file_object.write("\t".join(map(str,info_tuple)))
                 file_object.write("\n")
 
@@ -24,12 +31,12 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT username, scope FROM users")
+mycursor.execute("SELECT user_id, username, scope FROM users")
 
-myresult = mycursor.fetchall()
+users = mycursor.fetchall()
 
 filename = datetime.now().strftime("%Y-%m-%d.log")
 
-for x in myresult:
-    print(x)
-    updatelog(x[0], filename, x[1], x[1])
+for id, user, scope in users:
+    print(id, user, scope)
+    updatelog(id, user, filename, scope, scope)
